57ca4fd53d0c18d7bd58a7729b7674a8a679d766
Si11ycheese <si11ycheese6680@gmail.com>
2024-08-22 18:59:51 +0800

    Initial commit for cs106b

---
+++ CS106/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,8 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2023.1"
+#define SPL_BUILD_DATE "Thu Aug 22 12:48:27 2024"
+#define SPL_BUILD_USER ""
+
+#endif
---
+++ CS106_2021.1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Thu Aug 22 18:22:06 2024"
+#define SPL_BUILD_USER ""
+
+#endif
